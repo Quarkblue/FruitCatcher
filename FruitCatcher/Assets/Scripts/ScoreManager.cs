511f5f6a583e0ea96683658f5691e492020ce646
ViShhhhxx <visheshjain256@gmail.com>
2024-01-16 02:15:46 +0530

    FruitCatchercompleted

---
+++ FruitCatcher/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   
+using UnityEngine.SceneManagement;  
+using TMPro;
+using UnityEditor;
+
+public class ScoreManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    private int score;
+
+    public GameObject hat;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+        scoreText.text = score.ToString();  
+        
+    }
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Bomb")
+        {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);    
+        }   
+        
+    }
+
+    void OnTriggerExit2D(Collider2D target)
+    {
+        if(target.tag == "Fruit")
+        {
+            Destroy(target.gameObject);
+            score++;
+        }
+        
+    }
+    IEnumerator WaitTillRestart()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
