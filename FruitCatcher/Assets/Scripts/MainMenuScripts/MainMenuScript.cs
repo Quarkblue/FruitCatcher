ba2bfdac3beffd901d7478f751a10e8d38635e22
Divyansh Chauhan <106025754+Quarkblue@users.noreply.github.com>
2024-02-18 16:05:13 +0530

    audio fix

--- FruitCatcher/Assets/Scripts/MainMenuScripts/MainMenuScript.cs
+++ FruitCatcher/Assets/Scripts/MainMenuScripts/MainMenuScript.cs
@@ -7,10 +7,25 @@ using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject new_audSource = GameObject.Find("AudioManager");
+        if(new_audSource == null)
+        {
+            Destroy(new_audSource);
+        }
+        else
+        {
+            if (audioSource)
+            {
+                DontDestroyOnLoad(audioSource);
+            }
+        }
+
+        audioSource = GameObject.Find("AudioManager");
     }
 
     // Update is called once per frame
